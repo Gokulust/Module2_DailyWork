534ecb38172d930877c7db8d9cda658a91d20378
Gokulust <gokul.rajashekharan@ust.com>
2023-11-10 17:17:38 +0530

    Add project files.

---
+++ SeleniumExample/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium.DevTools.V117.WebAuthn;
+using NUnit.Framework;
+
+IWebDriver driver=new ChromeDriver();
+driver.Url="https://www.google.com";
+string title = driver.Title;
+Thread.Sleep(0000);
+try
+{
+    Assert.AreEqual("Gooogle", title);
+    Console.WriteLine("Pass");
+}
+catch(AssertionException)
+{
+    Console.WriteLine("Fail");
+}
+driver.Close();
